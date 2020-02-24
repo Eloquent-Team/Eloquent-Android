17d4e7ac0d39b0e868ccbc803244f3c791ea16b4
Maurice Gerhardt <maurice@mgcodes.one>
2020-02-24 12:44:29 +0100

    added data binding

--- app/src/main/java/berlin/eloquent/eloquentandroid/MainActivity.kt
+++ app/src/main/java/berlin/eloquent/eloquentandroid/MainActivity.kt
@@ -2,12 +2,14 @@ package berlin.eloquent.eloquentandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import berlin.eloquent.eloquentandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
