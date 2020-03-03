67d40d23290b4bcb1108207c6264213c259afeb3
Maurice Gerhardt <maurice@mgcodes.one>
2020-03-03 14:59:28 +0100

    MVVM finished for Recorder

--- app/src/main/java/berlin/eloquent/eloquentandroid/recorder/RecorderFragment.kt
+++ app/src/main/java/berlin/eloquent/eloquentandroid/recorder/RecorderFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import berlin.eloquent.eloquentandroid.R
 import berlin.eloquent.eloquentandroid.databinding.RecorderFragmentBinding
 
 class RecorderFragment : Fragment() {
@@ -17,8 +19,22 @@ class RecorderFragment : Fragment() {
         val binding = RecorderFragmentBinding.inflate(layoutInflater)
 
         viewModel = ViewModelProvider(this).get(RecorderViewModel::class.java)
+
         binding.recorderViewModel = viewModel
         binding.lifecycleOwner = this
+
+        viewModel.timeStamp.observe(viewLifecycleOwner, Observer { timestamp ->
+            viewModel.outputFile = activity?.getExternalFilesDir(null)?.absolutePath + "/recording_$timestamp"
+        })
+
+        viewModel.recordingPaused.observe(viewLifecycleOwner, Observer { recordingPaused ->
+            if (recordingPaused) {
+                binding.pauseRecording.setImageResource(R.drawable.ic_refresh)
+            } else {
+                binding.pauseRecording.setImageResource(R.drawable.ic_pause)
+            }
+
+        })
 
         return binding.root
     }
--- app/src/main/java/berlin/eloquent/eloquentandroid/recorder/RecorderViewModel.kt
+++ app/src/main/java/berlin/eloquent/eloquentandroid/recorder/RecorderViewModel.kt
@@ -3,10 +3,9 @@ package berlin.eloquent.eloquentandroid.recorder
 import android.media.MediaPlayer
 import android.media.MediaRecorder
 import android.util.Log
-import android.widget.Toast
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import berlin.eloquent.eloquentandroid.R
-import berlin.eloquent.eloquentandroid.databinding.RecorderFragmentBinding
 import java.io.IOException
 import java.time.Instant
 import java.time.ZoneOffset
@@ -15,16 +14,26 @@ import java.time.format.DateTimeFormatter
 class RecorderViewModel: ViewModel() {
 
     private var mediaRecorder: MediaRecorder? = null
-    private var outputFile = ""
+
+    var outputFile = ""
+
+    private val _timestamp = MutableLiveData<String>()
+    val timeStamp: LiveData<String> get() = _timestamp
+
     private var isRecording: Boolean = false
-    private var recordingPaused: Boolean = false
+
+    private val _recordingPaused = MutableLiveData<Boolean>()
+    val recordingPaused: LiveData<Boolean> get() = _recordingPaused
+
+    init {
+        _timestamp.value = ""
+        _recordingPaused.value = false
+    }
 
     fun startRecording() {
         Log.i("RecorderFragment", "fun startRecording called")
-        val timestamp = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss").withZone(ZoneOffset.UTC).format(
+        _timestamp.value = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss").withZone(ZoneOffset.UTC).format(
             Instant.now())
-
-        outputFile = activity?.getExternalFilesDir(null)?.absolutePath + "/recording_$timestamp"
         mediaRecorder = MediaRecorder().apply {
             setAudioSource(MediaRecorder.AudioSource.MIC)
             setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
@@ -37,48 +46,45 @@ class RecorderViewModel: ViewModel() {
                 Log.e("RecorderFragment", "prepare() failed")
             }
             start()
-            recordingPaused = false
+            _recordingPaused.value = false
             isRecording = true
-            Toast.makeText(activity, "Recording started!", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(activity, "Recording started!", Toast.LENGTH_SHORT).show()
         }
     }
 
-    fun stopRecording(binding: RecorderFragmentBinding) {
+    fun stopRecording() {
         if (isRecording) {
             mediaRecorder?.apply {
                 stop()
                 release()
             }
-            binding.pauseRecording.setImageResource(R.drawable.ic_pause)
             isRecording = false
             mediaRecorder = null
-            Toast.makeText(activity, "Recording stopped!", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(activity, "Recording stopped!", Toast.LENGTH_SHORT).show()
         }
     }
 
-    fun pauseRecording(binding: RecorderFragmentBinding) {
+    fun pauseRecording() {
         if (isRecording) {
-            if (!recordingPaused) {
-                Toast.makeText(activity, "Recording paused!", Toast.LENGTH_SHORT).show()
+            if (!_recordingPaused.value!!) {
+                //Toast.makeText(activity, "Recording paused!", Toast.LENGTH_SHORT).show()
                 mediaRecorder?.pause()
-                recordingPaused = true
-                binding.pauseRecording.setImageResource(R.drawable.ic_refresh)
+                _recordingPaused.value = true
             } else {
-                resumeRecording(binding)
+                resumeRecording()
             }
         }
     }
 
-    fun resumeRecording(binding: RecorderFragmentBinding) {
-        Toast.makeText(activity,"Recording resumed!", Toast.LENGTH_SHORT).show()
+    fun resumeRecording() {
+        //Toast.makeText(activity,"Recording resumed!", Toast.LENGTH_SHORT).show()
         mediaRecorder?.resume()
-        binding.pauseRecording.setImageResource(R.drawable.ic_pause)
-        recordingPaused = false
+        _recordingPaused.value = false
     }
 
     fun playRecording() {
         var mediaPlayer = MediaPlayer()
-        Toast.makeText(activity,"Recording is playing!", Toast.LENGTH_SHORT).show()
+        //Toast.makeText(activity,"Recording is playing!", Toast.LENGTH_SHORT).show()
         mediaPlayer.setDataSource(outputFile)
         mediaPlayer.prepare()
         mediaPlayer.start()
