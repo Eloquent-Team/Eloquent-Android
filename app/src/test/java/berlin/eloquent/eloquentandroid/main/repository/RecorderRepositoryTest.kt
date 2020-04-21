10d102707c5b332da6d23b00c3648adc3996923b
Maurice Gerhardt <maurice@mgcodes.one>
2020-04-21 17:25:09 +0200

    write new tests for RecorderRepository

--- app/src/test/java/berlin/eloquent/eloquentandroid/main/repository/RecorderRepositoryTest.kt
+++ app/src/test/java/berlin/eloquent/eloquentandroid/main/repository/RecorderRepositoryTest.kt
@@ -1,6 +1,8 @@
 package berlin.eloquent.eloquentandroid.main.repository
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import berlin.eloquent.eloquentandroid.MainCoroutineRule
 import berlin.eloquent.eloquentandroid.database.Recording
@@ -26,7 +28,16 @@ class RecorderRepositoryTest {
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
 
-    private val recording = Recording(
+    private val newRecording = Recording(
+        title = "TestRecording",
+        tags = "test more tests",
+        date = "2020-03-24 12:24:34",
+        length = 100L,
+        fileUrl = "fileUrl"
+    )
+
+    private val existingRecording = Recording(
+        recordingId = 2L,
         title = "TestRecording",
         tags = "test more tests",
         date = "2020-03-24 12:24:34",
@@ -44,9 +55,64 @@ class RecorderRepositoryTest {
     fun `insert Recording into db`() = runBlocking {
         every { dao.insertRecording(any()) } just runs
 
-        repository.insertRecording(recording)
+        repository.insertRecording(newRecording)
 
-        verify { dao.insertRecording(recording) }
+        verify { dao.insertRecording(newRecording) }
+
+        confirmVerified(dao)
+    }
+
+    @Test
+    fun `update Recording in db`() = runBlocking {
+        every { dao.updateRecording(any()) } just runs
+
+        repository.updateRecording(newRecording)
+
+        verify { dao.updateRecording(newRecording) }
+
+        confirmVerified(dao)
+    }
+
+    @Test
+    fun `delete Recording`() = runBlocking {
+        every { dao.deleteRecording(any()) } just runs
+
+        repository.deleteRecording(newRecording)
+
+        verify { dao.deleteRecording(newRecording) }
+
+        confirmVerified(dao)
+    }
+
+    @Test
+    fun `get Recording by id from db`() = runBlocking {
+        every { dao.getRecording(any()) } returns Recording()
+
+        repository.getRecording(existingRecording.recordingId)
+
+        verify { dao.getRecording(existingRecording.recordingId) }
+
+        confirmVerified(dao)
+    }
+
+    @Test
+    fun `get all Recordings from db`() = runBlocking {
+        every { dao.getAllRecordings() } returns MutableLiveData<List<Recording>>() as LiveData<List<Recording>>
+
+        repository.getAllRecordings()
+
+        verify { dao.getAllRecordings() }
+
+        confirmVerified(dao)
+    }
+
+    @Test
+    fun `get newest Recording from db`() = runBlocking {
+        every { dao.getNewestRecording() } returns Recording()
+
+        repository.getNewestRecording()
+
+        verify { dao.getNewestRecording() }
 
         confirmVerified(dao)
     }
