54ebfa5908f2050366b9561fe6c129963e1b8df2
Giuliano Fitzke <giuliano.fitzke@gmx.de>
2020-10-06 15:04:52 +0200

    Style cleanup

--- app/src/main/java/berlin/eloquent/eloquentandroid/main/feedback/retrofit/AudioService.kt
+++ app/src/main/java/berlin/eloquent/eloquentandroid/main/feedback/retrofit/AudioService.kt
@@ -6,14 +6,18 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
+const val BASE_URL = "http://161.35.197.123:8080/"
+const val ANALYZE_ENDPOINT = "api/v1/analyze/3"
+const val UPLOAD_ENDPOINT = ""
+
 interface AudioService {
-    @GET("api/v1/analyze/3")
+    @GET(ANALYZE_ENDPOINT)
     suspend fun getAnalysis(): Response<DataModel>
 
     companion object{
         fun create(): AudioService {
             return Retrofit.Builder()
-                .baseUrl("http://161.35.197.123:8080/")
+                .baseUrl(BASE_URL)
                 .client(OkHttpClient())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
