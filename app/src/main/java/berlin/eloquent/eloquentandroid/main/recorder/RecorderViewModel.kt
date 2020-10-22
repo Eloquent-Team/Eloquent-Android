5dba5663f9fc90727f5a05a02298b73ee080047e
SpringHawk <martin.zaubitzer@code.berlin>
2020-10-22 14:44:54 +0200

    Updated some gradle vars - Added raw Recording
    - added option to record raw audio files
    - store base64 encoded audio content in DB

--- app/src/main/java/berlin/eloquent/eloquentandroid/main/recorder/RecorderViewModel.kt
+++ app/src/main/java/berlin/eloquent/eloquentandroid/main/recorder/RecorderViewModel.kt
@@ -1,6 +1,8 @@
 package berlin.eloquent.eloquentandroid.main.recorder
 
 import android.app.Application
+import android.media.AudioFormat
+import android.media.AudioRecord
 import android.media.MediaRecorder
 import android.os.CountDownTimer
 import android.text.format.DateUtils
@@ -8,14 +10,22 @@ import android.util.Log
 import androidx.lifecycle.*
 import berlin.eloquent.eloquentandroid.database.Recording
 import berlin.eloquent.eloquentandroid.main.repository.IRecorderRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.io.IOException
+import java.io.File
+import java.io.FileOutputStream
 import java.time.Instant
 import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
+import java.util.*
 import javax.inject.Inject
 
-class RecorderViewModel @Inject constructor(private val repo: IRecorderRepository, val application: Application) : ViewModel() {
+private const val SAMPLE_RATE = 44100
+
+class RecorderViewModel @Inject constructor(
+        private val repo: IRecorderRepository,
+        val application: Application
+) : ViewModel() {
 
     // Attributes
     private var mediaRecorder: MediaRecorder? = null
@@ -41,10 +51,18 @@ class RecorderViewModel @Inject constructor(private val repo: IRecorderRepositor
         DateUtils.formatElapsedTime(time)
     }
 
+    lateinit var audioRecord: AudioRecord
+    var shouldAudioRecordContinue = true
+
+    private val minAudioBufferSize = AudioRecord.getMinBufferSize(
+            SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO,
+            AudioFormat.ENCODING_PCM_16BIT
+    )
+    private lateinit var fileCreationStream: FileOutputStream
 
     init {
         _recordingState.value =
-            RecordingState.NOT_STARTED
+                RecordingState.NOT_STARTED
         _currentTimeCode.value = 0L
         _outputFile.value = ""
     }
@@ -64,13 +82,74 @@ class RecorderViewModel @Inject constructor(private val repo: IRecorderRepositor
      *  AudioEncoder.AAC
      */
     private fun getConfiguredMediaRecorder(): MediaRecorder {
-        _outputFile.value = application.getExternalFilesDir(null)?.absolutePath + "/recording_${getCurrentTimestamp("yyyy-MM-dd_HH-mm-ss")}"
+        _outputFile.value = application.getExternalFilesDir(null)?.absolutePath + "/recording_${
+            getCurrentTimestamp("yyyy-MM-dd_HH-mm-ss")
+        }"
         return MediaRecorder().apply {
             setAudioSource(MediaRecorder.AudioSource.MIC)
             setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
             setOutputFile(_outputFile.value)
             setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
         }
+    }
+
+    /**
+     * Configures an AudioRecord Object
+     *
+     * Allows for recording of raw audio files.
+     */
+    private fun configureAudioRecord() {
+        audioRecord = AudioRecord.Builder()
+                .setAudioSource(MediaRecorder.AudioSource.DEFAULT)
+                .setAudioFormat(
+                        AudioFormat.Builder()
+                                .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+                                .setSampleRate(SAMPLE_RATE)
+                                .setChannelMask(AudioFormat.CHANNEL_IN_MONO)
+                                .build()
+                )
+                .setBufferSizeInBytes(minAudioBufferSize)
+                .build()
+    }
+
+    private fun startAudioRecord() {
+        configureAudioRecord()
+        _outputFile.value = application.getExternalFilesDir(null)?.absolutePath + "/recording_${
+            getCurrentTimestamp("yyyy-MM-dd_HH-mm-ss")
+        }.raw"
+
+        shouldAudioRecordContinue = true
+        viewModelScope.launch(Dispatchers.IO) {
+            var bufferSize = minAudioBufferSize
+
+            if (bufferSize == AudioRecord.ERROR || bufferSize == AudioRecord.ERROR_BAD_VALUE) {
+                bufferSize = SAMPLE_RATE * 2
+            }
+            val audioBuffer = ByteArray(bufferSize / 2)
+
+            if (audioRecord.state != AudioRecord.STATE_INITIALIZED) {
+                Log.e("Error", "Audio Record can't initialize!")
+                return@launch
+            }
+            audioRecord.startRecording()
+
+            Log.v("Success", "Start recording")
+            while (shouldAudioRecordContinue) {
+                audioRecord.read(audioBuffer, 0, audioBuffer.size)
+
+                val outputFile = File(_outputFile.value!!)
+                fileCreationStream = FileOutputStream(outputFile, true)
+                fileCreationStream.write(audioBuffer)
+            }
+        }
+    }
+
+    private fun stopAudioRecord() {
+        shouldAudioRecordContinue = false
+        audioRecord.stop()
+        audioRecord.release()
+        fileCreationStream.close()
+        Log.v("Success", "Recording stopped")
     }
 
     /**
@@ -113,25 +192,18 @@ class RecorderViewModel @Inject constructor(private val repo: IRecorderRepositor
     }
 
     private fun startRecording() {
-        mediaRecorder = getConfiguredMediaRecorder().apply {
-            try {
-                prepare()
-            } catch (e: IOException) {
-                Log.e("ViewModel Recorder", "prepare() failed")
-            }
-            start()
-            timer = getCountUpTimer(Long.MAX_VALUE)
-            _recordingState.value = RecordingState.RECORDING
-        }
+        startAudioRecord()
+        timer = getCountUpTimer(Long.MAX_VALUE)
+        _recordingState.value = RecordingState.RECORDING
     }
 
     private fun stopRecording() {
-        mediaRecorder?.apply {
-            stop()
-            release()
-        }
-        mediaRecorder = null
+        stopAudioRecord()
         timer.cancel()
+
+        val outputFileBytes = File(_outputFile.value!!).readBytes()
+        val content = Base64.getEncoder().encodeToString(outputFileBytes)
+
         viewModelScope.launch {
             _recording.value = Recording()
             _recording.value!!.apply {
@@ -139,6 +211,7 @@ class RecorderViewModel @Inject constructor(private val repo: IRecorderRepositor
                 date = getCurrentTimestamp("yyyy-MM-dd HH:mm:ss")
                 length = _currentTimeCode.value!!
                 fileUrl = _outputFile.value!!
+                base64Content = content
             }
             repo.insertRecording(_recording.value!!)
             _createdRecordingId.value = repo.getNewestRecording()!!.recordingId
